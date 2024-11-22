4564600108a512d2e0ab4e3cdea1ad2d46b49fbf
bingsuzoa <zxc_777@naver.com>
2024-11-23 02:37:01 +0900

    [권미경] #3 - 두번째 풀이 index초과로 계속 런타임오류

--- src/backjoon/quiz2346.java
+++ src/backjoon/quiz2346.java
@@ -2,7 +2,6 @@ package backjoon;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -14,9 +13,9 @@ public class quiz2346 {
         int N = Integer.parseInt(br.readLine());
         List<Integer> aList = new ArrayList<>();
         List<Integer> bList = new ArrayList<>();
-        int aIndex = 0;
-        int index2 = 0;
+        int index = 0;
         int value = 0;
+        int answer = 0;
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0; i < N; i++) {
@@ -24,27 +23,55 @@ public class quiz2346 {
             aList.add(M);
             bList.add(M);
         }
-        //1번째
-        //출력
-        bw.write(aIndex+1 + " ");
-        //index2의 value값 가져오기
-        value = bList.get(index2);
-        //다 저장했으면 삭제
-        bList.remove(index2);
+
+        //1번째 출력
+        bw.write(answer + 1 + " ");
+        //value 저장
+        value = bList.get(index);
+        //삭제
+        bList.remove(index);
 
         while(!bList.isEmpty()){
-            //새로운 index2 가져오기
-            index2 = Math.abs(index2-+value) % bList.size();
-            //index2의 value값 가져오기
-            value = bList.get(index2);
-            //value의 aList index가져오기
-            aIndex = aList.indexOf(value);
-            //출력
-            bw.write(aIndex+1 + " ");
-            //다 저장했으면 삭제
-            bList.remove(index2);
+            //이동
+            if(bList.size() > 1){
+                if(value > 0){
+                    if(index + value <= bList.size()-1){
+                        index = ((index-1) + value) % bList.size();
+                    } else {
+                        index = (index + value -1) % bList.size();
+                    }
+                } else {
+                    if(0<= index + value){
+                        index = index + value;
+                    } else {
+                        if(index == bList.size()){
+                            index = bList.size() + index + value;
+                        } else if (index == 0){
+                            if(Math.abs(index+value) % bList.size() == 0){
+                                index = 0;
+                            } else {
+                                index = bList.size() - (Math.abs(index+value) % bList.size());
+                            }
+                        } else {
+                            if(Math.abs(index+value) % bList.size() == 0){
+                                index = 0;
+                            } else {
+                                index = bList.size() - (Math.abs(index+value) % bList.size());
+                            }
+                        }
+                    }
+                }
+            } else {
+                index = 0;
+            }
+            //value값
+            value = bList.get(index);
+            //value값에 대한 aList index
+            answer = aList.indexOf(value);
+            bw.write(answer+1 + " ");
+            //삭제
+            bList.remove(index);
         }
-
         bw.flush();
         bw.close();
     }
