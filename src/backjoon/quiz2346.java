12e06386136a1f099cc3e47a97f797a28043a4cb
bingsuzoa <zxc_777@naver.com>
2024-11-25 01:22:11 +0900

    [권미경] #3 - deque활용하여 풀어보기

--- src/backjoon/quiz2346.java
+++ src/backjoon/quiz2346.java
@@ -1,9 +1,7 @@
 package backjoon;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class quiz2346 {
     public static void main(String[] args) throws IOException {
@@ -11,70 +9,41 @@ public class quiz2346 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
+        Deque<Integer> deque = new ArrayDeque<>();
+        List<Integer> list = new ArrayList<>();
         int index = 0;
         int value = 0;
-        int count = 1;
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0; i < N; i++) {
             int M = Integer.parseInt(st.nextToken());
-            arr[i] = M;
+            deque.add(M);
+            list.add(M);
         }
 
         //1번째 출력
         bw.write(index + 1 + " ");
         //value값
-        value = arr[index];
-        //0으로 초기화
-        arr[index] = 0;
+        value = list.get(index);
+        //삭제
+        deque.remove();
 
-        while(count < arr.length) {
+        while(!deque.isEmpty()) {
             //이동
             if (value > 0) {
-                for (int i = 1; i <= value; i++) {
-                    if (index + 1 <= arr.length - 1) {
-                        if (arr[index + 1] != 0) {
-                            index++;
-                        } else {
-                            index++;
-                            i--;
-                        }
-                    } else {
-                        if(arr[(index + 1) % arr.length] != 0){
-                            index = (index + 1) % arr.length;
-                        } else {
-                            index = (index + 1) % arr.length;
-                            i--;
-                        }
-                    }
+                for(int i = 0; i < value; i++){
+                    deque.addLast(deque.poll());
                 }
-            } else if (value < 0){
-                for (int i = 1; i <= Math.abs(value); i++) {
-                    if (index - 1 >= 0) {
-                        if (arr[index - 1] != 0) {
-                            index--;
-                        } else {
-                            index--;
-                            i--;
-                        }
-                    } else {
-                        if (arr[arr.length + index - 1] != 0) {
-                            index = arr.length + index - 1;
-                        } else {
-                            index = arr.length + index - 1;
-                            i--;
-                        }
-                    }
+                value = deque.pollLast();
+                index = list.indexOf(value);
+            } else {
+                for(int i = 0; i < Math.abs(value); i++){
+                    deque.addFirst(deque.pollLast());
                 }
+                value = deque.pollFirst();
+                index = list.indexOf(value);
             }
-            //value값 구하기
-            value = arr[index];
-            //출력
             bw.write(index + 1 + " ");
-            //초기화
-            arr[index] = 0;
-            count ++;
         }
         bw.flush();
         bw.close();
