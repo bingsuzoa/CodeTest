65fc3d3b584b4f2136fa9125341a432fb6383018
bingsuzoa <zxc_777@naver.com>
2024-11-25 22:04:51 +0900

    [권미경] #4 - 첫번째 풀이 : 시간초과

--- src/backjoon/quiz24511.java
+++ src/backjoon/quiz24511.java
@@ -1,7 +1,7 @@
 package backjoon;
 
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class quiz24511 {
 
@@ -11,24 +11,56 @@ public class quiz24511 {
         StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        int M = Integer.parseInt(br.readLine());
-        int[] A = new int[N];
-        int[] B = new int[N];
-        int[] C = new int[M];
 
+        st = new StringTokenizer(br.readLine());
+        int[] A = new int[N];
         for(int i = 0; i < N; i++){
             A[i] = Integer.parseInt(st.nextToken());
         }
+
+        st = new StringTokenizer(br.readLine());
+        int[] B = new int[N];
         for(int i = 0; i < N; i++){
             B[i] = Integer.parseInt(st.nextToken());
         }
+
+        int M = Integer.parseInt(br.readLine());
+        int[] C = new int[M];
+        st = new StringTokenizer(br.readLine());
         for(int i = 0; i < M; i++){
             C[i] = Integer.parseInt(st.nextToken());
         }
 
+        Deque<Integer> deque = new ArrayDeque<>();
+        Stack<Integer> stack = new Stack<>();
 
 
+
+        //deque에 값 넣기
+        for(int i = 0; i < N; i++){
+            deque.add(B[i]);
+        }
+        for(int i = 0; i < M; i++) {
+            int c = C[i];
+            for (int j = 0; j < A.length; j++) {
+                //0이면 큐
+                if (A[j] == 0) {
+                    for (int k = 0; k <= j; k++) {
+                        stack.add(deque.pollFirst());
+                    }
+                    deque.addFirst(c);
+                    c = stack.pop();
+                    for (int w = 0; w < stack.size(); w++) {
+                        deque.addFirst(stack.pop());
+                    }
+                    //1이면 stack
+                } else {
+                    continue;
+                }
+            }
+            bw.write(c + " ");
+        }
+        bw.flush();
+        bw.close();
     }
 }
