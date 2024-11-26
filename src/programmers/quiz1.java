4ff92323da88d04d29070a8570a79472848d2476
bingsuzoa <zxc_777@naver.com>
2024-11-27 02:07:32 +0900

    [권미경] #6 - 첫번째 풀이 오답

---
+++ src/programmers/quiz1.java
@@ -0,0 +1,62 @@
+package programmers;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class quiz1 {
+    public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:SS");
+        String answer = "";
+
+        Date date1 = sdf.parse(video_len);
+        long vLength = date1.getTime();
+
+        Date date2 = sdf.parse(pos);
+        long cTime = date2.getTime();
+
+        Date date3 = sdf.parse(op_start);
+        long osTime = date3.getTime();
+
+        Date date4 = sdf.parse(op_end);
+        long oeTime = date4.getTime();
+
+        Date date5 = sdf.parse("00:10");
+        long tenSec = date5.getTime();
+
+        for(int i = 0; i < commands.length; i++){
+            String command = commands[i];
+            if(command.equals("prev")){
+                //10초 이하
+                if(cTime <= tenSec){
+                    answer = "00:00";
+                } else {
+                //10초 초과
+                    long ansTime = cTime-tenSec;
+                    long ansSec = ansTime / 1000;
+                    long ansMin = ansTime / (1000 * 60);
+                    answer = ansMin + ":" + ansSec;
+                }
+            } else if(command.equals("next")) {
+                //종료시간 10초
+                if(cTime + tenSec >= vLength){
+                    answer = video_len;
+                }
+                //오프닝 끝난 후, 오프닝 시작까지 10초남음
+                if(cTime >= oeTime || cTime + tenSec < osTime){
+                    long ansTime = cTime+tenSec;
+                    long ansSec = ansTime / 1000;
+                    long ansMin = ansTime / (1000 * 60);
+                    answer = ansMin + ":" + ansSec;
+                } else {
+                    long ansTime = oeTime + tenSec;
+                    long ansSec = ansTime / 1000;
+                    long ansMin = ansTime / (1000 * 60);
+                    answer = ansMin + ":" + ansSec;
+                }
+
+            }
+        }
+        return answer;
+    }
+}
