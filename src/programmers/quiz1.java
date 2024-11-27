decff5a430b4c3690f425b0b51d8ae458f84629c
bingsuzoa <zxc_777@naver.com>
2024-11-27 15:18:04 +0900

    [권미경] #6 - 테스트오류

--- src/programmers/quiz1.java
+++ src/programmers/quiz1.java
@@ -1,70 +1,93 @@
 package programmers;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
 
 public class quiz1 {
     public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) throws ParseException {
 
-       LocalTime date1 = LocalTime.of(00, Integer.parseInt(video_len.split(":")[0]), Integer.parseInt(video_len.split(":")[1]));
+       LocalTime date1 = LocalTime.of(0, Integer.parseInt(video_len.split(":")[0]), Integer.parseInt(video_len.split(":")[1]));
        int vLength = date1.getMinute() * 60 + date1.getSecond();
 
-       LocalTime date2 = LocalTime.of(00, Integer.parseInt(pos.split(":")[0]), Integer.parseInt(pos.split(":")[1]));
-       int cMin = date2.getSecond();
+       LocalTime date2 = LocalTime.of(0, Integer.parseInt(pos.split(":")[0]), Integer.parseInt(pos.split(":")[1]));
        int cTime = date2.getMinute() * 60 + date2.getSecond();
 
-       LocalTime date3 = LocalTime.of(00, Integer.parseInt(op_start.split(":")[0]), Integer.parseInt(op_start.split(":")[1]));
+       LocalTime date3 = LocalTime.of(0, Integer.parseInt(op_start.split(":")[0]), Integer.parseInt(op_start.split(":")[1]));
        int osTime = date3.getMinute() * 60 + date3.getSecond();
 
-       LocalTime date4 = LocalTime.of(00, Integer.parseInt(op_end.split(":")[0]), Integer.parseInt(op_end.split(":")[1]));
+       LocalTime date4 = LocalTime.of(0, Integer.parseInt(op_end.split(":")[0]), Integer.parseInt(op_end.split(":")[1]));
        int oeTime = date4.getMinute() * 60 + date4.getSecond();
 
-       LocalTime date5 = LocalTime.of(00,00,00);
+       LocalTime date5 = LocalTime.of(0,0,0);
        int sTime = date5.getMinute() * 60 + date5.getSecond();
 
-        String answer = "";
-        long tenSec = 10;
+        String answer;
 
         for(int i = 0; i < commands.length; i++){
             String command = commands[i];
             if(command.equals("prev")){
-                //10초 이하
-                if(cTime <= 10){
+                //오프닝 구간 전
+                if(cTime < osTime && cTime - 10 < 10){
                     cTime = sTime;
-                } else {
-                //10초 초과
-                    cTime = cTime + 10;
+                } else if(cTime < osTime){
+                    cTime = cTime - 10;
+                }
+                //오프닝 구간
+                else if(cTime <= oeTime + 10){
+                    cTime = oeTime;
+                }
+                //오프닝 지난 구간
+                else {
+                    cTime = cTime - 10;
                 }
             } else if(command.equals("next")) {
                 //종료시간 10초
-                if(cTime + 10 >= vLength){
+                if(cTime + 10 > vLength){
                     cTime = vLength;
                 }
-                //오프닝 끝난 후, 오프닝 시작까지 10초남음
-                if(cTime >= oeTime || cTime + 10 < osTime){
+                //오프닝 후 < ㅁ < 종료 10초
+                else if(oeTime < cTime){
                     cTime = cTime + 10;
-                } else {
+                }
+                //오프닝 구간
+                else if(osTime <= cTime){
                     cTime = oeTime + 10;
+                //오프닝 전 구간
+                } else if(cTime + 10 < osTime){
+                    cTime = cTime + 10;
+                } else if(cTime + 10 >= osTime && cTime + 10 <= oeTime){
+                    cTime = oeTime;
+                } else if(cTime + 10 > oeTime){
+                    cTime = cTime + 10;
                 }
 
             }
         }
-        int ansSec = (int)(cTime % 60);
-        int ansMin = (int)(cTime / 60);
-        answer = ansMin + ":" + ansSec;
+        int ansSec = cTime % 60;
+        String ansSec2;
+        String ansMin2;
+        if(ansSec < 10){
+            ansSec2 = "0" + ansSec;
+        } else {
+            ansSec2 = String.valueOf(ansSec);
+        }
 
+        int ansMin = cTime / 60;
+        if(ansMin < 10){
+            ansMin2 = "0" + ansMin;
+        } else {
+            ansMin2 = String.valueOf(ansMin);
+        }
+        answer = ansMin2 + ":" + ansSec2;
         return answer;
     }
 
     public static void main(String[] args){
-        String video_len = "34:33";
-        String pos = "13:00";
-        String op_start = "00:55";
-        String op_end = "02:55";
-        String[] commands = {"next", "prev"};
+        String video_len = "30:00";
+        String pos = "00:08";
+        String op_start = "00:00";
+        String op_end = "00:05";
+        String[] commands = {"prev"};
 
         try {
             quiz1 solution1 = new quiz1();
