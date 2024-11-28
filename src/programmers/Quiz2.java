1a22a57c3efe3106aed12383dac5b074dfc5d51a
bingsuzoa <zxc_777@naver.com>
2024-11-28 14:56:44 +0900

    [권미경] #7 - 시간초과, 런타임에러

--- src/programmers/Quiz2.java
+++ src/programmers/Quiz2.java
@@ -2,30 +2,101 @@ package programmers;
 
 public class Quiz2 {
     public int solution(int[] diffs, int[] times, long limit) {
-        int answer;
+        int answer = 0;
         long result = 0;
-        int level = 1;
+        int level;
 
-        while(true){
-            for(int i = 0; i<diffs.length; i++){
-                int diff = diffs[i];
-                if(level >= diff){
-                    result += times[i];
-                } else {
-                    result += (times[i] + ((long)(times[i] + times[i-1]) * (diff-level)));
-                }
-            }
-            if(result > limit) {
-                result = 0;
-                level++;
+        //한계 result찾기
+        //배열의 최댓값
+       int max = 1;
+       int maxLevel = 1;
+       for(int i = 1; i < diffs.length; i++){
+           if(max < diffs[i]){
+               max = diffs[i];
+           }
+       }
+       //result 구하기 : level이 배열의 최댓값일때
+        level = max;
+        maxLevel = max;
+        for(int i = 0; i < diffs.length; i++){
+            int diff = diffs[i];
+            if(level >= diff){
+                result += times[i];
             } else {
-                break;
+                result += (times[i] + ((long)(times[i] + times[i-1])*(diff-level)));
             }
         }
-        answer = level;
+        //이진트리로 줄여가야한다.
+        int minLevel = 0;
+        while (true) {
+            if(result <= limit) {
+                result = 0;
+                level = (minLevel + level) / 2;
+                for (int i = 0; i < diffs.length; i++) {
+                    int diff = diffs[i];
+                    if (level >= diff) {
+                        result += times[i];
+                    } else {
+                        result += (times[i] + ((long) (times[i] + times[i - 1]) * (diff - level)));
+                    }
+                }
+                if (result <= limit){
+                    result = 0;
+                    level = level -1;
+                    for (int i = 0; i < diffs.length; i++) {
+                        int diff = diffs[i];
+                        if (level >= diff) {
+                            result += times[i];
+                        } else {
+                            result += (times[i] + ((long) (times[i] + times[i - 1]) * (diff - level)));
+                        }
+                    }
+                    if(result > limit){
+                        level = level + 1;
+                        answer = level;
+                        break;
+                    } else {
+                        maxLevel = level;
+                        continue;
+                    }
+                }
+            }
+            minLevel = level;
+            if(result > limit){
+                result = 0;
+                level = (minLevel + maxLevel) /2;
+                for (int i = 0; i < diffs.length; i++) {
+                    int diff = diffs[i];
+                    if (level >= diff) {
+                        result += times[i];
+                    } else {
+                        result += (times[i] + ((long) (times[i] + times[i - 1]) * (diff - level)));
+                    }
+                }
+                if (result <= limit){
+                    result = 0;
+                    level = level -1;
+                    for (int i = 0; i < diffs.length; i++) {
+                        int diff = diffs[i];
+                        if (level >= diff) {
+                            result += times[i];
+                        } else {
+                            result += (times[i] + ((long) (times[i] + times[i - 1]) * (diff - level)));
+                        }
+                    }
+                    if(result > limit){
+                        level = level + 1;
+                        answer = level;
+                        break;
+                    } else {
+                        maxLevel = level;
+                        continue;
+                    }
+                }
+            }
+        }
         return answer;
     }
-
     public static void main(String[] args){
         int[] diffs = {1,99999,100000,99995};
         int[] times = {9999,9001,9999,9001};
