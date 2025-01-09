c30382886233e839302f57d23f75908b8220c36a
bingsuzoa <zxc_777@naver.com>
2025-01-09 12:13:40 +0900

    [권미경] 1.9. 목 두번째풀이

--- src/programmers/InterceptionSystem.java
+++ src/programmers/InterceptionSystem.java
@@ -5,10 +5,12 @@ import java.util.*;
 public class InterceptionSystem {
     private static int max;
     private static int min;
-    private static List<List<Integer>> list = new ArrayList<>(max+1);
+    private static List<Integer> list = new ArrayList<>();
+    private static Map<Integer, List<Integer>> map = new HashMap<>();
+    private static int maxValue;
 
     public static void main(String[] args) {
-        int[][] targets = {{4,5},{4,8},{10,14},{11,13},{5,12},{3,7},{1,4}};
+        int[][] targets = {{1,2}};
         InterceptionSystem test = new InterceptionSystem();
         System.out.println(test.solution(targets));
     }
@@ -18,55 +20,67 @@ public class InterceptionSystem {
         max = getMax(targets);
         min = getMin(targets);
 
-        for(int i = 0; i < max + 1; i++) {
-            list.add(new ArrayList<>());
-        }
-
         for(int i = 0; i < targets.length; i++) {
-            list.get(targets[i][0]).add(targets[i][1]);
-        }
-
-        for(int i = 0; i < list.size(); i++) {
-            if(list.get(i).isEmpty()) {
-                continue;
+            if(!map.containsKey(targets[i][0])) {
+                map.put(targets[i][0], new ArrayList());
+                map.get(targets[i][0]).add(targets[i][1]);
             } else {
-                Collections.sort(list.get(i));
+                map.get(targets[i][0]).add(targets[i][1]);
             }
         }
 
-        int maxValue = -1;
-        int minIndex = min;
-        while(maxValue != max) {
-            List<Integer> tempList = list.get(minIndex);
-            int maxIndex = tempList.get(0);
-
-            for(int i = minIndex + 1; i < maxIndex; i++) {
-                if(!list.get(i).isEmpty()) {
-                    minIndex = i;
-                    break;
-                }
-            }
-
-            int minValue = 100000001;
-            for(int i = minIndex; i < maxIndex; i++) {
-                if(list.get(i).isEmpty()) {
-                    continue;
-                } else {
-                    for(int j = 0; j < list.get(i).size(); j++) {
-                        minValue = Math.min(minValue, list.get(i).get(j));
-                        maxValue = Math.max(maxValue, list.get(i).get(j));
-                    }
-                }
-            }
-            if(minValue < maxIndex) {
-                maxIndex = minValue;
-            }
-            answer ++;
-            minIndex = maxIndex;
+        for(Integer key : map.keySet()) {
+            list.add(key);
+            Collections.sort(map.get(key));
         }
+        Collections.sort(list);
+
+        int index = 0;
+        int x = list.get(index);
+        int y = map.get(x).get(0);
+
+        while(true) {
+            int[] result = getRange(x,y);
+            x = result[0];
+            maxValue = result[1];
+            y = map.get(x).get(0);
+
+            if(maxValue == max) {
+                answer++;
+                break;
+            } else {
+                answer++;
+            }
+        }
+
         return answer;
     }
+    public int[] getRange(int x, int y) {
+        int max_x = x;
+        int min_y = y;
+        int start = list.indexOf(x);
+        for(int i = start; i < list.size(); i++) {
+            if(list.get(i) > y) {
+                break;
+            }
+            else if(list.get(i) >= x && list.get(i) < y && list.get(i) < min_y) {
+                max_x = Math.max(max_x, list.get(i));
+                List<Integer> tempList = map.get(list.get(i));
+                min_y = Math.min(min_y, tempList.get(0));
+                maxValue = Math.max(maxValue, tempList.get(tempList.size()-1));
+            }
+        }
 
+        int index = list.indexOf(max_x);
+        for(int i = index + 1; i < list.size(); i++) {
+            if(list.get(i) >= min_y) {
+                max_x = list.get(i);
+                break;
+            }
+        }
+        int[] range = {max_x, maxValue};
+        return range;
+    }
     public int getMax(int[][] targets) {
         int max = 0;
         for(int i = 0; i < targets.length; i++) {
@@ -84,6 +98,4 @@ public class InterceptionSystem {
         }
         return min;
     }
-
-
 }
