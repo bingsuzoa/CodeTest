a6de5518ee7f99ccb3debcbbf92f608763f745b0
bingsuzoa <zxc_777@naver.com>
2025-01-11 10:48:18 +0900

    [권미경] #13 - 이진탐색으로 풀어보기

--- src/programmers/InterceptionSystem.java
+++ src/programmers/InterceptionSystem.java
@@ -3,101 +3,75 @@ package programmers;
 import java.util.*;
 
 public class InterceptionSystem {
-    private static int max;
-    private static int min;
-    private static List<Integer> list = new ArrayList<>();
-    private static Map<Integer, List<Integer>> map = new HashMap<>();
-    private static int maxValue;
+//    private static int[][] targets = {{1,7},{3,4},{3,8},{6,20},{8,9},{12,22},{23,24}};
+    private static int[][] targets = {{1,4},{2,3},{5,12},{10,11},{11,13}};
 
     public static void main(String[] args) {
-        int[][] targets = {{1,5},{3,20},{4,15},{5,8},{6,18}};
         InterceptionSystem test = new InterceptionSystem();
         System.out.println(test.solution(targets));
     }
 
     public int solution(int[][] targets) {
+        Map<Integer, Integer> map = new HashMap<>();
         int answer = 0;
-        max = getMax(targets);
-        min = getMin(targets);
 
         for(int i = 0; i < targets.length; i++) {
             if(!map.containsKey(targets[i][0])) {
-                map.put(targets[i][0], new ArrayList());
-                map.get(targets[i][0]).add(targets[i][1]);
+                map.put(targets[i][0], targets[i][1]);
             } else {
-                map.get(targets[i][0]).add(targets[i][1]);
+                int min = map.get(targets[i][0]);
+                min = Math.min(min, targets[i][1]);
+                map.replace(targets[i][0], min);
             }
         }
 
-        for(Integer key : map.keySet()) {
+        List<Integer> list = new ArrayList<>();
+        for(int key : map.keySet()) {
             list.add(key);
-            Collections.sort(map.get(key));
         }
         Collections.sort(list);
 
-        int index = 0;
-        int x = list.get(index);
-        int y = map.get(x).get(0);
-
-        while(true) {
-            int[] result = getRange(x,y);
-            x = result[0];
-            maxValue = result[1];
-            index = result[2];
-            y = map.get(x).get(0);
-
-            if(maxValue == max && index == list.size() - 1) {
+        int index_bef = 0;
+        while(index_bef < list.get(list.size()-1)) {
+            int start = list.get(index_bef);
+            int end = map.get(start);
+            int index_next = getIndex(list, index_bef, end, end);
+            if(index_next - index_bef >= 2) {
+                for(int i = index_bef + 1; i < index_next; i++) {
+                    if(map.get(i) <= list.get(i+1)) {
+                        answer++;
+                        index_bef = index_next;
+                    }
+                }
+            }
+            else if(index_next == list.size() -1) {
                 answer++;
-                break;
+            }
+        }
+        return answer+1;
+    }
+
+    public int getIndex(List<Integer> list, int index_bef, int end, int target) {
+        int result = 0;
+        while(index_bef <= end) {
+            int mid = (index_bef + end) / 2;
+            if(target > list.get(mid)) {
+                index_bef = mid + 1;
+            }
+            else if(target == list.get(mid)) {
+                result = mid;
+                return result;
+
             } else {
-                answer++;
+                if(target > list.get(mid -1)) {
+                    result = mid;
+                    return result;
+                } else {
+                    end = mid - 1;
+                }
             }
         }
-
-        return answer;
-    }
-    public int[] getRange(int x, int y) {
-        int max_x = x;
-        int min_y = y;
-        int start = list.indexOf(x);
-        for(int i = start; i < list.size(); i++) {
-            if(list.get(i) > y) {
-                break;
-            }
-            else if(list.get(i) >= x && list.get(i) < y && list.get(i) < min_y) {
-                max_x = Math.max(max_x, list.get(i));
-                List<Integer> tempList = map.get(list.get(i));
-                min_y = Math.min(min_y, tempList.get(0));
-                maxValue = Math.max(maxValue, tempList.get(tempList.size()-1));
-            }
-        }
-
-        int index = list.indexOf(max_x);
-        for(int i = index + 1; i < list.size(); i++) {
-            if(list.get(i) >= min_y) {
-                max_x = list.get(i);
-                index = i;
-                break;
-            }
-        }
-        int[] range = {max_x, maxValue, index};
-        return range;
-    }
-    public int getMax(int[][] targets) {
-        int max = 0;
-        for(int i = 0; i < targets.length; i++) {
-            max = Math.max(max, targets[i][0]);
-            max = Math.max(max, targets[i][1]);
-        }
-        return max;
+        return list.size() - 1;
     }
 
-    public int getMin(int[][] targets) {
-        int min = 100000001;
-        for(int i = 0; i < targets.length; i++) {
-            min = Math.min(min, targets[i][0]);
-            min = Math.min(min, targets[i][1]);
-        }
-        return min;
-    }
 }
