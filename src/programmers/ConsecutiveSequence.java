4f07aec50644d9f1c17332c4cfe7c0eeab92f963
bingsuzoa <zxc_777@naver.com>
2025-01-20 14:23:19 +0900

    [권미경] #21 - 1.20.(월)

--- src/programmers/ConsecutiveSequence.java
+++ src/programmers/ConsecutiveSequence.java
@@ -1,11 +1,15 @@
 package programmers;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class ConsecutiveSequence {
+    PriorityQueue<Index> pq = new PriorityQueue<>(1, new IndexComparator());
 
     public static void main(String[] args) {
         ConsecutiveSequence test = new ConsecutiveSequence();
-        int[] sequence = {1, 1, 1, 2, 3, 4, 5};
-        int k = 5;
+        int[] sequence = {2,2,2,2,2,2};
+        int k = 6;
         int[] answer = test.solution(sequence, k);
         for(int value : answer) {
             System.out.print(value + " ");
@@ -15,67 +19,108 @@ public class ConsecutiveSequence {
     public int[] solution(int[] sequence, int k) {
         int[] answer = new int[2];
 
-        for(int i = 0; i < sequence.length; i++) {
-            if(k == sequence[i]) {
-                answer[0] = i;
-                answer[1] = i;
-                return answer;
-            }
+        int temp = findK(sequence, k);
+        if(temp != -1) {
+            answer[0] = temp;
+            answer[1] = temp;
+            return answer;
         }
-
-        int[] temp = twoCount(sequence, k);
-        if(sequence[temp[0]] + sequence[temp[1]] == k) {
-            return temp;
-        } else {
-            int mid = temp[1];
-            for(int i = mid; i >= 1; i--) {
-                int sum = 0;
-                sum += sequence[mid];
-                int index = i;
-                while(sum < k) {
-                    index--;
-                    if(index < 0) {
-                        break;
-                    }
-                    sum += sequence[index];
-                }
-                if(sum == k) {
-                    answer[0] = index;
-                    answer[1] = i;
-                }
-            }
-
-        }
-
+        findIndex(sequence, k);
+        Index index = pq.poll();
+        answer[0] = index.startIndex;
+        answer[1] = index.endIndex;
         return answer;
     }
 
-    public int[] twoCount(int[] sequence, int k) {
-        int[] index = new int[2];
-        double half = k / (double) 2;
+    public int findK(int[] sequence, int k) {
+        int answer = -1;
         int start = 0;
         int end = sequence.length -1;
         while(start <= end) {
             int mid = (start + end) / 2;
-            if(mid == sequence.length - 1){
-                index[1] = end;
-                return index;
-            }
-            if(sequence[mid] < half && sequence[mid + 1] > half) {
-                if(sequence[mid] + sequence[mid + 1] == k) {
-                    index[0] = mid;
-                    index[1] = mid+1;
-                    return index;
-                } else {
-                    index[0] = 0;
-                    index[1] = mid;
-                    return index;
-                }
-            }
-            else if(sequence[mid] < half && sequence[mid + 1] < half) {
+            if(sequence[mid] < k) {
                 start = mid + 1;
             }
+            else if(sequence[mid] == k) {
+                answer = mid;
+                return answer;
+            }
+            else {
+                end = mid - 1;
+            }
         }
-        return index;
+        return answer;
+    }
+    public void findIndex(int[] sequence, int k) {
+        int halfKIndex = findIndexOverThanHalfK(sequence, k);
+        int startIndex = halfKIndex + 1;
+        int endIndex;
+        int sum;
+        while(startIndex >= 1) {
+            startIndex--;
+            sum = sequence[startIndex];
+            for(int i = startIndex; i >= 1; i--) {
+                int temp = sequence[i-1];
+                if(sum + temp > k) {
+                    break;
+                } else if(sum + temp == k) {
+                    endIndex = i-1;
+                    pq.add(new Index(endIndex, startIndex));
+                    break;
+                } else {
+                    sum += temp;
+                }
+            }
+        }
+    }
+
+    public int findIndexOverThanHalfK(int[] sequence, int k) {
+        double halfK = (double)k / 2;
+        int start = 0;
+        int end = sequence.length - 1;
+        while(start <= end) {
+            int mid = (start + end) / 2;
+            if(sequence[mid] < halfK) {
+                start = mid + 1;
+            }
+            else if(sequence[mid] > halfK) {
+                end = mid - 1;
+            }
+            else {
+                return mid;
+            }
+        }
+        if(start == sequence.length) {
+            return start -1;
+        }
+        return start+1;
+    }
+}
+class Index {
+    int startIndex;
+    int endIndex;
+    public Index(int startIndex, int endIndex) {
+        this.startIndex = startIndex;
+        this.endIndex = endIndex;
+    }
+}
+class IndexComparator implements Comparator<Index> {
+    @Override
+    public int compare(Index o1, Index o2) {
+        int size_o1 = o1.endIndex - o1.startIndex;
+        int size_o2 = o2.endIndex - o2.startIndex;
+        if(size_o1 < size_o2) {
+            return size_o1 - size_o2;
+        }
+        else if(size_o1 > size_o2) {
+            return size_o1 - size_o2;
+        }
+        else {
+            if(o1.startIndex < o2.startIndex) {
+                return o1.startIndex - o2.startIndex;
+            } else {
+                return o2.startIndex - o1.startIndex;
+            }
+        }
     }
 }
