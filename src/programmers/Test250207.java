477d74c0cffd9adb974326584ec44e8e469e07f9
bingsuzoa <zxc_777@naver.com>
2025-02-07 16:40:55 +0900

    [권미경] #29 25.02.07

--- src/programmers/Test250207.java
+++ src/programmers/Test250207.java
@@ -1,60 +1,71 @@
 package programmers;
 
 import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Test250207 {
-    static boolean[][] visited;
+    static int M;
+    static int N;
+    static int[][] graph;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
-        Test250207 test = new Test250207();
 
         String input = br.readLine();
         st = new StringTokenizer(input);
-        int M = Integer.parseInt(st.nextToken());
-        int N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
 
-        int[][] graph = new int[M][N];
-        visited = new boolean[M][N];
-
-        for(int i = 0; i < M; i++) {
+        graph = new int[M][N];
+        for (int i = 0; i < M; i++) {
             String input2 = br.readLine();
             st = new StringTokenizer(input2);
-            while(st.hasMoreTokens()) {
-                for(int j = 0; j < graph[0].length; j++) {
+            while (st.hasMoreTokens()) {
+                for (int j = 0; j < N; j++) {
                     graph[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
         }
-        int count = 0;
-        for(int i = 0; i < graph.length; i++) {
-            for(int j = 0; j < graph[0].length; j++) {
-                if(visited[i][j] || graph[i][j] == 1) {
-                    continue;
-                }
-                count += test.findZero(graph, i, j);
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(0,0, 1));
+
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
+        while(!queue.isEmpty()) {
+            Node node = queue.poll();
+            int x = node.x;
+            int y = node.y;
+            int value = node.value;
+            if (x >= M || x <= -1 || y >= N || y <= -1 || graph[x][y] != 1) {
+                continue;
+            }
+
+            graph[x][y] = value;
+
+            for(int i = 0; i < dx.length; i++) {
+                queue.add(new Node(x + dx[i], y+ dy[i], graph[x][y]+1));
             }
         }
-        bw.write(count + "");
+
+
+        bw.write(graph[M-1][N-1] + "");
         bw.close();
     }
+}
 
-    public int findZero(int[][] graph, int i, int j) {
-        if(i >= graph.length || i < 0 || j >= graph[0].length || j < 0 || visited[i][j]) {
-            return 0;
-        }
-        visited[i][j] = true;
-        if(graph[i][j] == 0) {
-            findZero(graph, i+1, j);
-            findZero(graph, i, j+1);
-            findZero(graph, i-1, j);
-            findZero(graph, i, j-1);
-        } else {
-            return 0;
-        }
-        return 1;
+class Node {
+    int x;
+    int y;
+    int value;
+
+    Node(int x, int y, int value) {
+        this.x = x;
+        this.y = y;
+        this.value = value;
     }
+
 }
