67fc669e939895ed23c8e28b10aa45f798dd1456
bingsuzoa <zxc_777@naver.com>
2025-02-08 12:32:40 +0900

    [권미경] #30 05.02.08 : A도시에서 연결된 모든 도시를 확인한 후 진행해야 하므로 BFS queue 활용

--- src/programmers/Test250206.java
+++ src/programmers/Test250206.java
@@ -1,79 +1,63 @@
 package programmers;
 
-import java.util.*;
+
 
 public class Test250206 {
+    int min = 50 * 25;
 
     public static void main(String[] args) {
+        String[] minerals = {"diamond", "diamond", "diamond", "iron", "iron", "diamond", "iron", "stone"};
+        int[] picks = {1,3,2};
         Test250206 test = new Test250206();
-        int[] picks = {0, 1, 1};
-        String[] minerals = {"diamond", "diamond", "diamond", "diamond", "diamond", "iron", "iron", "iron", "iron", "iron", "diamond"};
-        System.out.println(test.solution(picks, minerals));
+        System.out.println(test.solution(picks,minerals));
     }
 
     public int solution(int[] picks, String[] minerals) {
-        int answer = Integer.MAX_VALUE;
-        int[][] tired = new int[3][3];
+        self(minerals, 0, picks[0], picks[1], picks[2], 0);
+        return min;
+    }
 
-        for(int i = 0; i < tired.length; i++) {
-            for(int j = 0; j < tired[i].length; j++) {
-                tired[i][j] = 1;
-            }
+    private void self(String[] minerals, int start, int diamond, int iron, int stone, int sum) {
+
+        if(minerals.length <= start) {
+            min = Math.min(min, sum);
+            return;
         }
-        tired[1][0] = 5;
-        tired[2][0] = 25;
-        tired[2][1] = 5;
 
-        Queue<int[][]> changedPicks = makeRandomPicks(picks);
-        while(!changedPicks.isEmpty()) {
-            Queue<Integer> changedMinerals = stringToInt(minerals);
-            int[][] newPicks = changedPicks.poll();
-            int sum = 0;
-            for(int i = 0; i < newPicks.length; i++) {
-                int count = newPicks[i][1] * 5;
-                while(count > 0 && !changedMinerals.isEmpty()) {
-                    sum += tired[newPicks[i][0]][changedMinerals.poll()];
-                    count--;
+        if(diamond == 0 && iron == 0 && stone == 0) {
+            min = Math.min(min, sum);
+            return;
+        }
+
+        int length = Math.min(minerals.length, start + 5);
+
+        if(diamond > 0) {
+            int sub = length - start;
+            self(minerals, length, diamond-1, iron, stone, sum+sub);
+        }
+        if(iron > 0) {
+            int sub = 0;
+            for(int i = start; i < length; i++) {
+                if(minerals[i].equals("diamond")) {
+                    sub += 5;
+                } else {
+                    sub++;
                 }
             }
-            if(answer > sum) {
-                answer = sum;
-            }
+            self(minerals, length, diamond, iron-1, stone, sum+sub);
         }
-        return answer;
-    }
-
-    public Queue<Integer> stringToInt(String[] minerals) {
-        Queue<Integer> changeMinerals = new LinkedList<>();
-
-        for(String mineral : minerals) {
-            if(mineral.equals("diamond")) {
-                changeMinerals.add(0);
+        if(stone > 0) {
+            int sub = 0;
+            for(int i = start; i < length; i++) {
+                if(minerals[i].equals("diamond")) {
+                    sub += 25;
+                } else if(minerals[i].equals("iron")) {
+                    sub += 5;
+                } else {
+                    sub ++;
+                }
             }
-            else if(mineral.equals("iron")) {
-                changeMinerals.add(1);
-            }
-            else {
-                changeMinerals.add(2);
-            }
+            self(minerals, length, diamond, iron, stone-1, sum + sub);
         }
-        return changeMinerals;
-    }
-
-    public Queue<int[][]> makeRandomPicks(int[] picks) {
-        Queue<int[][]> queue = new LinkedList<>();
-
-        int[] diamond = {0, picks[0]};
-        int[] iron = {1, picks[1]};
-        int[] stone = {2, picks[2]};
-
-        queue.add(new int[][] {diamond, iron, stone});
-        queue.add(new int[][] {diamond, stone, iron});
-        queue.add(new int[][] {iron, stone, diamond});
-        queue.add(new int[][] {iron, diamond, stone});
-        queue.add(new int[][] {stone, diamond, iron});
-        queue.add(new int[][] {stone, iron, diamond});
-
-        return queue;
     }
 }
