e6e576d6ff63bdd1bcc733d878878305c4d8fa3e
bingsuzoa <zxc_777@naver.com>
2025-02-10 13:42:54 +0900

    [권미경] #31 25.02.10 : 리코쳇로봇에서 상하좌우 모두 불가능한경우 빠져나오지를 못함

--- src/programmers/Test250210.java
+++ src/programmers/Test250210.java
@@ -1,39 +1,76 @@
 package programmers;
 
-import java.io.*;
-import java.util.StringTokenizer;
 
 public class Test250210 {
-    private static int count_1;
-    private static int count_2;
+    private static int m, n;
+    private static final int[] dx = {1,-1,0,0};
+    private static final int[] dy = {0,0,1,-1};
+    private static boolean changeLock = false;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Test250210 test = new Test250210();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] board = {"...D..R", ".D.G...", "....D.D", "D....D.", "..D...."};
+        System.out.println(test.solution(board));
 
-        String input = br.readLine();
-        String standard = input.substring(0,1);
-        for(int i = 1; i < input.length(); i++) {
-            if(!standard.equals(input.substring(i, i+1))) {
-                count_1++;
-                i = test.findIndex(i, input) -1;
-                if(i == input.length()) break;
-                if(input.substring(input.length() -1).equals(standard)) count_1++;
-            }
-        }
-        System.out.println(count_1 + "," + count_2);
-        System.out.println(Math.min(count_1, count_2));
     }
 
-    public int findIndex(int i, String input) {
-        String standard = input.substring(i, i+1);
-        for(int j = i+1; j < input.length(); j++) {
-            if(!input.substring(j, j+1).equals(standard)) {
-                count_2++;
-                return j;
+    public int solution(String[] board) {
+        m = board.length;
+        n = board[0].length();
+        char[][] reBoard = getReBoard(board);
+
+        int sum = find(reBoard, 0, n-1, -1, changeLock, 0);
+        if(sum == 0) return -1;
+        return sum;
+    }
+
+    public int find(char[][] reBoard, int i, int j, int index, boolean changeLock, int sum) {
+        if(i >= m || j >= n || i <= -1 || j <= -1) {
+            return 0;
+        }
+
+
+        if(!changeLock) {
+            if(index == 0 || index == 1) {
+                find(reBoard, i + dx[2], j + dy[2], 2, true, sum);
+                find(reBoard, i + dx[3], j + dy[3], 3, true, sum);
+            }
+            else if(index == 2 || index == 3) {
+                find(reBoard, i + dx[0], j + dy[0], 0, true, sum);
+                find(reBoard, i + dx[1], j + dy[1], 1, true, sum);
+            } else {
+                for(int k = 0; k < 4; k++) {
+                    find(reBoard, i + dx[k], j + dy[k], k, true, sum);
+                }
             }
         }
-        count_2++;
-        return input.length();
+        else {
+            if(reBoard[i][j] == 'D') {
+                if(reBoard[i-dx[index]][j-dy[index]] == 'G') {
+                    return sum+1;
+                }
+                find(reBoard, i-dx[index], j-dy[index], index, false, sum+1);
+            }
+            else if((i== m-1 || i == 0) && (j==n-1 || j == 0)) {
+                if(reBoard[i][j] == 'G') {
+                    return sum +1;
+                }
+                find(reBoard, i, j, index, false, sum+1);
+            } else {
+                reBoard[i][j] = 'V';
+                find(reBoard, i+dx[index], j+dy[index], index, true, sum);
+            }
+        }
+        return 0;
+    }
+    
+    public char[][] getReBoard(String[] board) {
+        char[][] reBoard = new char[m][n];
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                reBoard[i][j] = board[i].charAt(j);
+            }
+        }
+        return reBoard;
     }
 }
