e1780ca999fee7bf4f26cd42169961d44d3588a3
bingsuzoa <zxc_777@naver.com>
2025-02-13 12:53:27 +0900

    #34 25.02.13 : 리코쳇로봇에서 visited를 int로 설정한 후 가는 방향에 대한 정보를 넣었다. 같은 방향이거나 대칭인 방향인 경우 넘어가도록 설정, 테스트 코드만 맞음...

--- src/Solution.java
+++ src/Solution.java
@@ -1,87 +1,150 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.util.LinkedList;
+import java.util.Queue;
+
 
 public class Solution {
+    static int m,n;
+    static char[][] graph;
+    static int min = Integer.MAX_VALUE;
+    static Queue<Node> que = new LinkedList<>();
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        String[] board = {".D.R", "....", ".G..", "...D"};
+        Solution test = new Solution();
+        System.out.println(test.solution(board));
+    }
 
-        String input = br.readLine();
-        st = new StringTokenizer(input);
-        int m = Integer.parseInt(st.nextToken());
-        int n = Integer.parseInt(st.nextToken());
+    public int solution(String[] board) {
+        m = board.length;
+        n = board[0].length();
+        graph = getBoard(board);
 
-        int[][] graph = new int[m][n];
-        Node[][] f = new Node[m][n];
-        String input2 = br.readLine();
-        st = new StringTokenizer(input2);
-        while(st.hasMoreTokens()) {
-            for(int i = 0; i < m; i++) {
-                for(int j = 0; j < n; j++) {
-                    graph[i][j] = Integer.parseInt(st.nextToken());
-                }
-            }
-        }
-        for(int x = 0; x < m; x++) {
-            f[x][0] = new Node(x, 0);
-            for(int i = 1; i < n; i++) {
-                Node node = f[x][i-1];
-                if(node.x == 0) {
-                    int max = Math.max(graph[node.x][node.y+1], graph[node.x+1][node.y+1]);
-                    if(graph[node.x][node.y+1] == max) {
-                        f[x][i] = new Node(node.x, node.y+1);
-                    }
-                    if(graph[node.x+1][node.y+1] == max) {
-                        f[x][i] = new Node(node.x+1, node.y+1);
-                    }
-                }
-                else if(node.x == m-1) {
-                    int max = Math.max(graph[node.x][node.y+1], graph[node.x-1][node.y+1]);
-                    if(graph[node.x][node.y+1] == max) {
-                        f[x][i] = new Node(node.x, node.y+1);
-                    }
-                    if(graph[node.x-1][node.y+1] == max) {
-                        f[x][i] = new Node(node.x-1, node.y+1);
-                    }
-                }
-                else {
-                    int max = graph[node.x][node.y+1];
-                    max = Math.max(max, graph[node.x+1][node.y+1]);
-                    max = Math.max(max, graph[node.x-1][node.y+1]);
-                    if(graph[node.x][node.y+1] == max) {
-                        f[x][i] = new Node(node.x, node.y+1);
-                    }
-                    if(graph[node.x+1][node.y+1] == max) {
-                        f[x][i] = new Node(node.x+1, node.y+1);
-                    }
-                    if(graph[node.x-1][node.y+1] == max) {
-                        f[x][i] = new Node(node.x-1, node.y+1);
-                    }
-                }
-            }
-        }
-
-        int max = 0;
+        int[][] visited = new int[m][n];
         for(int i = 0; i < m; i++) {
-            int sum = 0;
             for(int j = 0; j < n; j++) {
-                Node node = f[i][j];
-                sum += graph[node.x][node.y];
+                visited[i][j] = -1;
             }
-            max = Math.max(max, sum);
         }
-        System.out.println(max);
+
+        que.add(new Node(0, n-1, 0, visited));
+        while(!que.isEmpty()) {
+            Node node = que.poll();
+            int x = node.x;
+            int y = node.y;
+            int sum = node.sum;
+            if(x >= m || y >= n || x < 0 || y < 0) {
+                continue;
+            }
+            if(graph[x][y] == 'G') {
+                min = Math.min(min, sum);
+            } else {
+                for(int i = 0; i < 4; i++) {
+                    go(x, y, i, node.visited, sum+1);
+                }
+            }
+        }
+        if(min == Integer.MAX_VALUE) {
+            return -1;
+        } else {
+            return min;
+        }
+    }
+
+    public void go(int x, int y, int index, int[][] visited, int sum) {
+        int[][] newVisited = new int[m][n];
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                newVisited[i][j] = visited[i][j];
+            }
+        }
+        if(index == 0) {
+            for(int i = x+1; i < m; i++) {
+                if(newVisited[i][y] == index || newVisited[i][y] == 1 || graph[i][y] == 'R') {
+                    break;
+                }
+                newVisited[i][y] = index;
+                if(graph[i][y] == 'D') {
+                    que.add(new Node(i-1, y, sum, newVisited));
+                    break;
+                }
+                if(i == m-1) {
+                    que.add(new Node(i, y, sum, newVisited));
+                    break;
+                }
+            }
+        }
+        if(index == 1) {
+            for(int i = x-1; i >= 0; i--) {
+                if(newVisited[i][y] == index || newVisited[i][y] == 0 || graph[i][y] == 'R') {
+                    break;
+                }
+                newVisited[i][y] = index;
+                if(graph[i][y] == 'D') {
+                    que.add(new Node(i+1, y, sum, newVisited));
+                    break;
+                }
+                if(i == 0) {
+                    que.add(new Node(i, y, sum, newVisited));
+                    break;
+                }
+            }
+        }
+        if(index == 2) {
+            for(int i = y+1; i < n; i++) {
+                if(newVisited[x][i] == index || newVisited[x][i] == 3 || graph[x][i] == 'R') {
+                    break;
+                }
+                newVisited[x][i] = index;
+                if(graph[x][i] == 'D') {
+                    que.add(new Node(x, i-1, sum, newVisited));
+                    break;
+                }
+                if(i == n-1) {
+                    que.add(new Node(x, i, sum, newVisited));
+                    break;
+                }
+            }
+        }
+        if(index == 3) {
+            for(int i = y-1; i >= 0; i--) {
+                if(newVisited[x][i] == index || newVisited[x][i] == 2  || graph[x][i] == 'R') {
+                    break;
+                }
+                newVisited[x][i] = index;
+                if(graph[x][i] == 'D') {
+                    que.add(new Node(x, i+1, sum, newVisited));
+                    break;
+                }
+                if(i == 0) {
+                    que.add(new Node(x, i, sum, newVisited));
+                    break;
+                }
+            }
+        }
+    }
+
+    public char[][] getBoard(String[] board) {
+        char[][] graph = new char[m][n];
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                graph[i][j] = board[i].charAt(j);
+            }
+        }
+        return graph;
     }
 }
+
 class Node {
     int x;
     int y;
+    int sum;
+    int[][] visited;
 
-    Node(int x, int y) {
+    Node(int x, int y, int sum, int[][] visited) {
         this.x = x;
         this.y = y;
+        this.sum = sum;
+        this.visited = visited;
     }
 }
