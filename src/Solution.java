3e6b7f5eed3d6c937c2e0e0598aec6797cb3b76d
bingsuzoa <zxc_777@naver.com>
2025-02-14 12:50:24 +0900

    #35 25.02.14 : 비용을 고려해야한다면 Node에서 Comparable을 overriding하여 비용을 우선순위로 한다. 1-> 2 , 2-> 3 이런식으로 구현하고자하면 우선순위큐에 담기

--- src/Solution.java
+++ src/Solution.java
@@ -1,82 +1,79 @@
-import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Solution {
 
-    public static void main(String[] args) {
-        Solution test = new Solution();
-        int[][] input = {{1,2,2},{1,3,3},{1,4,1},{1,5,10},{2,4,2}, {3,4,1},{3,5,1},{4,5,3},{3,5,10}, {3,1,8},{1,4,2},{5,1,7}
-                        ,{3,4,2},{5,2,4}};
-        int[][] graph = test.getGraph(input);
-        Stack<Node> stack = test.getStack(graph);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        while(!stack.isEmpty()) {
-            Node node = stack.pop();
-            int x = node.x;
-            int y = node.y;
+        String input = br.readLine();
+        st = new StringTokenizer(input);
+        int c = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
 
-            for(int i = 1; i < graph[0].length; i++) {
-                if(x == i)  {
-                    graph[x][i] = 0;
-                }
-                if(graph[y][i] == 100001) {
-                    continue;
-                }
-                graph[x][i] = Math.min(graph[x][i], graph[x][y] + graph[y][i]);
+        List<ArrayList<Node>> graph = new ArrayList<>();
+        for(int i = 0; i <= c; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for(int i = 0; i < r; i++) {
+            String input2 = br.readLine();
+            st = new StringTokenizer(input2);
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int count = Integer.parseInt(st.nextToken());
+            graph.get(start).add(new Node(end,count));
+        }
+
+        int[] city = new int[c+1];
+        for(int i = 0; i < city.length; i++) {
+            city[i] = Integer.MAX_VALUE;
+        }
+
+        city[s] = 0;
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(s, 0));
+
+        while(!pq.isEmpty()) {
+            Node node = pq.poll();
+            int index = node.index;
+            int distance = node.distance;
+
+            for(int i = 0; i < graph.get(index).size(); i++) {
+                Node subNode = graph.get(index).get(i);
+                city[subNode.index] = Math.min(city[subNode.index], distance + subNode.distance);
+                pq.add(new Node(subNode.index, subNode.distance));
             }
         }
 
-        for(int i = 1; i < graph.length; i++) {
-            for(int j = 1; j < graph[0].length; j++) {
-                System.out.print(graph[i][j] + " ");
-            }
-            System.out.println();
-        }
-
-    }
-
-    public int[][] getGraph(int[][] input) {
-        int[][] graph = new int[6][6];
-
-        for(int i = 0; i < graph.length; i++) {
-            for(int j = 0; j < graph[0].length; j++) {
-                graph[i][j] = 100001;
+        int maxCount = 0;
+        int maxCity = 0;
+        for(int i = 0; i < city.length; i++) {
+            if(city[i] != Integer.MAX_VALUE) {
+                maxCount = Math.max(maxCount, city[i]);
+                maxCity++;
             }
         }
-        for(int i = 0; i < input.length; i++) {
-            int x = input[i][0];
-            int y = input[i][1];
-            int value = input[i][2];
-            if(graph[x][y] == 0) {
-                graph[x][y] = value;
-            } else {
-                graph[x][y] = Math.min(graph[x][y], value);
-            }
-        }
-        return graph;
-    }
-
-    public Stack<Node> getStack(int[][] graph) {
-        Stack<Node> stack = new Stack<>();
-        for(int i = 0; i < graph.length; i++) {
-            for(int j = 0; j < graph[0].length; j++) {
-                if(graph[i][j] != 100001) {
-                    stack.add(new Node(i,j));
-                }
-            }
-        }
-        return stack;
+        System.out.println(maxCity-1 + " " + maxCount);
     }
 }
-class Node {
-    int x;
-    int y;
 
-    Node(int x, int y) {
-        this.x = x;
-        this.y = y;
+class Node implements Comparable<Node> {
+    int index;
+    int distance;
+
+    Node(int index, int distance) {
+        this.index = index;
+        this.distance = distance;
+    }
+
+    @Override
+    public int compareTo(Node other) {
+        return this.distance - other.distance;
     }
 }
+
