34debc5336e3c64cf7417310e16711008e759bd5
bingsuzoa <zxc_777@naver.com>
2025-02-15 17:24:45 +0900

    #35 25.02.14 도시분할계획 : 최소한의 비용으로 도시를 분할하고 싶을때는 모두 연결해놓고, 제일 비용이 큰 도로를 생략하면 된다.

--- src/Solution.java
+++ src/Solution.java
@@ -1,56 +1,86 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Solution {
-    static int[][] graph = {{},{0, 1, 0, 1, 1}, {1, 0, 1, 1, 0}, {0, 1, 0, 0, 0}, {1, 1, 0, 0, 0}, {1, 0, 0, 0, 0}};
-    static int[] plan = {2, 3, 4, 3};
+    static int[] graph;
 
-    public static void main(String[] args) {
-        Solution test = new Solution();
-        int[] parent = new int[6];
-        for(int i = 0; i < 6; i++) {
-            parent[i] = i;
+    public static void main(String[] args) throws IOException {
+         Solution test = new Solution();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        String input = br.readLine();
+        st = new StringTokenizer(input);
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        PriorityQueue<Node> queue = new PriorityQueue<>();
+        for (int i = 0; i < m; i++) {
+            String input2 = br.readLine();
+            st = new StringTokenizer(input2);
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            queue.add(new Node(x,y,cost));
         }
 
-        for(int i = 1; i < parent.length; i++) {
-            int index = parent[i];
+        graph = new int[n+1];
+        for(int i = 1; i < n+1; i++) {
+            graph[i] = i;
+        }
 
-            for(int j = 1; j < graph[index].length; j++) {
-                if(graph[index][j] != 0) {
-                    parent = test.union(parent, index, j);
-                }
+        int sum = 0;
+        int max = 0;
+        Set<Integer> set = new HashSet<>();
+        while(!queue.isEmpty()) {
+            Node node = queue.poll();
+            int x = node.x;
+            int y = node.y;
+            int cost = node.cost;
+            if(test.find(graph, x) == test.find(graph, y)) {
+                continue;
             }
-        }
+            sum += cost;
+            max = Math.max(max, cost);
+            graph = test.union(graph, x, y);
 
-        boolean possible = true;
-        for(int i = 0; i < plan.length - 1; i++) {
-            if(parent[plan[i]] != parent[plan[i+1]]) {
-                possible = false;
-            }
         }
-        if(possible) {
-            System.out.println("Yes");
+        System.out.println(sum - max);
+    }
+    public int[] union(int[] graph, int x, int y) {
+        int nx = find(graph, x);
+        int ny = find(graph, y);
+        if(nx < ny) {
+            graph[ny] = nx;
         } else {
-            System.out.println("No");
+            graph[nx] = ny;
         }
+        return graph;
     }
 
-    public int[] union(int[] parent, int x, int y) {
-        x = findParent(parent, x);
-        y = findParent(parent, y);
-
-        if(x < y) {
-            parent[y] = x;
-        } else {
-            parent[x] = y;
+    public int find(int[] graph, int x) {
+        if(graph[x] != x) {
+            return graph[x] = find(graph, graph[x]);
         }
-        return parent;
+        return graph[x];
+    }
+}
+
+class Node implements Comparable<Node> {
+    int x;
+    int y;
+    int cost;
+
+    Node(int x, int y, int cost) {
+        this.x = x;
+        this.y = y;
+        this.cost = cost;
     }
 
-    public int findParent(int[] parent, int x) {
-        if(parent[x] != x) {
-            findParent(parent, parent[x]);
-        }
-        return parent[x];
+    @Override
+    public int compareTo(Node other) {
+        return this.cost - other.cost;
     }
 }
