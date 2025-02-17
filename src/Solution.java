181f6ceb396e08a6019042e3a6429377f0d57bda
bingsuzoa <zxc_777@naver.com>
2025-02-17 12:13:15 +0900

    #35 25.02.17 당구연습 : 연산만 잘하면되는 문제인데 부동소수점때문에 올바른 연산이 잘 안되는 상태

--- src/Solution.java
+++ src/Solution.java
@@ -1,60 +1,82 @@
-import java.io.*;
-import java.util.*;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Solution {
+    static int max_m;
+    static int max_n;
+    static int min = Integer.MAX_VALUE;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-
-        int n = Integer.parseInt(br.readLine());
-        int[] result = new int[n+1];
-        int[] time = new int[n+1];
-        List<ArrayList<Integer>> list = new ArrayList<>(n+1);
-        int[] order = new int[n+1];
-
-        for(int i = 0; i < n+1; i++) {
-            list.add(new ArrayList<>());
+    public static void main(String[] args) {
+        Solution test = new Solution();
+        int[][] balls = {{7,7},{2,7},{7,3}};
+        int[] answer = test.solution(10,10,3,7,balls);
+        for(int value : answer) {
+            System.out.println(value);
         }
-
-        Queue<Integer> queue =  new LinkedList<>();
-        for(int i = 1; i <= n; i++) {
-            String input = br.readLine();
-            st = new StringTokenizer(input);
-            time[i] = Integer.parseInt(st.nextToken());
-            while(st.hasMoreTokens()) {
-                int value = Integer.parseInt(st.nextToken());
-                if(value == -1) {
-                    continue;
-                }
-                list.get(value).add(i);
-                order[i] ++;
-            }
-        }
-
-        for(int i = 1; i < order.length; i++) {
-            if(order[i] == 0) {
-                result[i] = time[i];
-                queue.add(i);
-            }
-        }
-
-        while(!queue.isEmpty()) {
-            int now = queue.poll();
-            int now_time = result[now];
-
-            for(int i = 0; i < list.get(now).size(); i++) {
-                order[list.get(now).get(i)]--;
-                result[list.get(now).get(i)] = Math.max(result[list.get(now).get(i)], now_time + time[list.get(now).get(i)]);
-                if(order[list.get(now).get(i)] == 0) {
-                    queue.add(list.get(now).get(i));
-                }
-            }
-        }
-        for(int i = 1; i < result.length; i++) {
-            System.out.println(result[i]);
-        }
-
     }
+
+    public int[] solution(int m, int n, int startX, int startY, int[][] balls) {
+        max_m = m;
+        max_n = n;
+        int[] answer = new int[balls.length];
+
+        for(int i = 0; i < balls.length; i++) {
+            min = Integer.MAX_VALUE;
+            min = Math.min(min, measure(startX, startY, balls[i][0], balls[i][1], "LEFT"));
+            min = Math.min(min, measure(startX, startY, balls[i][0], balls[i][1], "RIGHT"));
+            min = Math.min(min, measure(startX, startY, balls[i][0], balls[i][1], "UP"));
+            min = Math.min(min, measure(startX, startY, balls[i][0], balls[i][1], "BOTTOM"));
+            answer[i] = min;
+        }
+        return answer;
+    }
+
+    public int measure(int sx, int sy, int x, int y, String direction) {
+        if (direction.equals("LEFT")) {
+            if(sy == y) {
+                return Integer.MAX_VALUE;
+            }
+            double sum_1 = (x * x) + Math.pow(Math.round((y-sy) / (double)2), 2);
+            double sum_2 = (sx * sx) + Math.pow(Math.round((y-sy) / (double)2), 2);
+            sum_1 = Math.round(Math.sqrt(sum_1));
+            sum_2 = Math.round(Math.sqrt(sum_2));
+            double result = Math.pow(sum_1 + sum_2, 2);
+            return (int)Math.round(result);
+        }
+        if (direction.equals("RIGHT")) {
+            if(sy == y) {
+                return Integer.MAX_VALUE;
+            }
+            double sum_1 = Math.pow(max_m-sx, 2) + Math.pow(((y - sy) / (double)2), 2);
+            double sum_2 = Math.pow(max_m-sx, 2) + Math.pow(((y - sy) / (double)2), 2);
+            sum_1 = Math.sqrt(sum_1);
+            sum_2 = Math.sqrt(sum_2);
+            double result = Math.pow(sum_2 + sum_1, 2);
+            return (int)Math.round(result);
+        }
+        if (direction.equals("UP")) {
+            if(sx == x) {
+                return Integer.MAX_VALUE;
+            }
+            double sum_1 = Math.pow((sx-x) / (double)2, 2) + Math.pow(max_n-y,2);
+            double sum_2 = Math.pow((sx-x) / (double)2, 2) + Math.pow(max_n-sy, 2);
+            sum_1 = Math.sqrt(sum_1);
+            sum_2 = Math.sqrt(sum_2);
+            double result = Math.pow(sum_2 + sum_1, 2);
+            return (int)Math.round(result);
+        }
+        if (direction.equals("BOTTOM")) {
+            if(sx == x) {
+                return Integer.MAX_VALUE;
+            }
+            double sum_1 = Math.pow((sx-x) / (double)2, 2) + Math.pow(y,2);
+            double sum_2 = Math.pow((sx-x) / (double)2, 2) + Math.pow(sy,2);
+            sum_1 = Math.sqrt(sum_1);
+            sum_2 = Math.sqrt(sum_2);
+            double result = Math.pow(sum_2 + sum_1, 2);
+            return (int)Math.round(result);
+        }
+        return 0;
+    }
+
 }
