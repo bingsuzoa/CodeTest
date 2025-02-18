9616153c65fcf369b36136051325dbcf06f54b91
bingsuzoa <zxc_777@naver.com>
2025-02-18 11:43:41 +0900

    #35 25.02.18 만들 수 없는 금액 : 현재 coin의 값이 target보다 작거나 같으면 만들 수 있고, target + coin -1까지 모두 만들 수 있다까지 성립한다.

--- src/Solution.java
+++ src/Solution.java
@@ -1,67 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Solution {
-    static int max_m;
-    static int max_n;
-    static int min = Integer.MAX_VALUE;
 
     public static void main(String[] args) {
-        Solution test = new Solution();
-        int[][] balls = {{7, 7}, {2, 7}, {7, 3}};
-        int[] answer = test.solution(10, 10, 3, 7, balls);
-        for (int value : answer) {
-            System.out.println(value);
-        }
-    }
+        Scanner sc = new Scanner(System.in);
 
-    public int[] solution(int m, int n, int startX, int startY, int[][] balls) {
-        max_m = m;
-        max_n = n;
-        int[] answer = new int[balls.length];
+        int n = sc.nextInt();
+        int[] coin = new int[n];
+        for(int i = 0; i < n; i++) {
+            coin[i] = sc.nextInt();
+        }
 
-        for (int i = 0; i < balls.length; i++) {
-            min = Integer.MAX_VALUE;
-            min = Math.min(min, measure(startX, startY, balls[i][0], balls[i][1], "LEFT"));
-            min = Math.min(min, measure(startX, startY, balls[i][0], balls[i][1], "RIGHT"));
-            min = Math.min(min, measure(startX, startY, balls[i][0], balls[i][1], "UP"));
-            min = Math.min(min, measure(startX, startY, balls[i][0], balls[i][1], "BOTTOM"));
-            answer[i] = min;
-        }
-        return answer;
-    }
+        Arrays.sort(coin);
 
-    public int measure(int sx, int sy, int x, int y, String direction) {
-        if (direction.equals("LEFT")) {
-            if(y == sy) {
-                return Integer.MAX_VALUE;
+        int target = 1;
+        for(int i = 0; i < coin.length; i++) {
+            if(coin[i] <= target) {
+                target += coin[i];
+            } else {
+                System.out.println(target);
+                break;
             }
-            sx = -sx;
-            int result = (x - sx) * (x - sx) + (y - sy) * (y - sy);
-            return result;
         }
-        if (direction.equals("RIGHT")) {
-            if(y == sy) {
-                return Integer.MAX_VALUE;
-            }
-            sx = 2 * (max_n - sx) + sx;
-            int result = (x - sx) * (x - sx) + (y - sy) * (y - sy);
-            return result;
-        }
-        if (direction.equals("UP")) {
-            if(x == sx) {
-                return Integer.MAX_VALUE;
-            }
-            sy = 2 * (max_m - sy) + sy;
-            int result = (x - sx) * (x - sx) + (y - sy) * (y - sy);
-            return result;
-        }
-        if (direction.equals("BOTTOM")) {
-            if(x == sx) {
-                return Integer.MAX_VALUE;
-            }
-            sy = -sy;
-            int result = (x - sx) * (x - sx) + (y - sy) * (y - sy);
-            return result;
-        }
-        return 0;
     }
 }
