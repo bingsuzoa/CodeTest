0bf6b9a244e9b88a728466437763d654222cca34
bingsuzoa <zxc_777@naver.com>
2025-02-18 15:34:03 +0900

    #35 25.02.18 자물쇠와 열쇠 : 회전까지 구현완료, 이동 구현필요

--- src/Solution.java
+++ src/Solution.java
@@ -1,80 +1,24 @@
 import java.util.*;
 
 public class Solution {
-    static List<Integer> list = new ArrayList<>();
-    static Queue<Node> queue = new LinkedList<>();
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
+    public boolean solution(int[][] key, int[][] lock) {
+        boolean answer = true;
 
-    public static void main(String[] args) {
-        Solution test = new Solution();
-        int n = 5;
-        String input = "R R R U D D";
-        StringTokenizer st = new StringTokenizer(input);
-        while (st.hasMoreTokens()) {
-            String direction = st.nextToken();
-            if(direction.equals("D")) {
-                list.add(0);
-            }
-            if(direction.equals("U")) {
-                list.add(1);
-            }
-            if(direction.equals("R")) {
-                list.add(2);
-            }
-            if(direction.equals("L")) {
-                list.add(3);
-            }
-        }
-        queue.add(new Node(0, 0, 0));
-        while (!queue.isEmpty()) {
-            Node node = queue.poll();
-            int x = node.x;
-            int y = node.y;
-            int plan = node.plan;
+        int[][] key_90 = rotate(key);
+        int[][] key_180 = rotate(key_90);
+        int[][] key_270 = rotate(key_180);
 
-            if (x >= n || y >= n || x < 0 || y < 0) {
-                int before_plan = list.get(plan-1);
-                x = x -dx[before_plan];
-                y = y - dy[before_plan];
-            }
-            if (plan == list.size()) {
-                x++;
-                y++;
-                System.out.println(x + " " + y);
-            } else {
-                test.move(x, y, plan);
-            }
-        }
-
-
+        return answer;
     }
 
-    public void move(int x, int y, int plan) {
-        int direction = list.get(plan);
-        if (direction == 2) {
-            queue.add(new Node(x + dx[2], y + dy[2], plan + 1));
+    public int[][] rotate(int[][] key) {
+        int m = key.length;
+        int[][] newKey = new int[m][m];
+        for (int i = 0; i < m; i++) {
+            for (int j = m - 1; j >= 0; j--) {
+                newKey[i][m - 1 - j] = key[j][i];
+            }
         }
-        if (direction == 3) {
-            queue.add(new Node(x + dx[3], y + dy[3], plan + 1));
-        }
-        if (direction == 1) {
-            queue.add(new Node(x + dx[1], y + dy[1], plan + 1));
-        }
-        if (direction == 0) {
-            queue.add(new Node(x + dx[0], y + dy[0], plan + 1));
-        }
-    }
-}
-
-class Node {
-    int x;
-    int y;
-    int plan;
-
-    Node(int x, int y, int plan) {
-        this.x = x;
-        this.y = y;
-        this.plan = plan;
+        return newKey;
     }
 }
