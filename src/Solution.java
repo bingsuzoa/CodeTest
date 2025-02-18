a8ee9f7e2a9240bcd928d37f2e96485f06d076fe
bingsuzoa <zxc_777@naver.com>
2025-02-18 15:47:00 +0900

    #35 25.02.18 당구연습 : 일직선상에 있다고 해서 다 안되는거 아님. x,y좌표 제대로 볼것(x = m, y = n)인데 반대로 생각해서 테케 다틀림

--- src/Solution.java
+++ src/Solution.java
@@ -1,24 +1,59 @@
-import java.util.*;
+
 
 public class Solution {
-    public boolean solution(int[][] key, int[][] lock) {
-        boolean answer = true;
+    static int max_m;
+    static int max_n;
+    static int min = Integer.MAX_VALUE;
 
-        int[][] key_90 = rotate(key);
-        int[][] key_180 = rotate(key_90);
-        int[][] key_270 = rotate(key_180);
+    public int[] solution(int m, int n, int startX, int startY, int[][] balls) {
+        max_m = m;
+        max_n = n;
+        int[] answer = new int[balls.length];
 
+        for (int i = 0; i < balls.length; i++) {
+            min = Integer.MAX_VALUE;
+            min = Math.min(min, measure(startX, startY, balls[i][0], balls[i][1], "LEFT"));
+            min = Math.min(min, measure(startX, startY, balls[i][0], balls[i][1], "RIGHT"));
+            min = Math.min(min, measure(startX, startY, balls[i][0], balls[i][1], "UP"));
+            min = Math.min(min, measure(startX, startY, balls[i][0], balls[i][1], "BOTTOM"));
+            answer[i] = min;
+        }
         return answer;
     }
 
-    public int[][] rotate(int[][] key) {
-        int m = key.length;
-        int[][] newKey = new int[m][m];
-        for (int i = 0; i < m; i++) {
-            for (int j = m - 1; j >= 0; j--) {
-                newKey[i][m - 1 - j] = key[j][i];
+    public int measure(int sx, int sy, int x, int y, String direction) {
+        if (direction.equals("LEFT")) {
+            if (y == sy && x < sx) {
+                return Integer.MAX_VALUE;
             }
+            sx = -sx;
+            int result = (x - sx) * (x - sx) + (y - sy) * (y - sy);
+            return result;
         }
-        return newKey;
+        if (direction.equals("RIGHT")) {
+            if (y == sy && sx < x) {
+                return Integer.MAX_VALUE;
+            }
+            sx = 2 * max_m - sx;
+            int result = (x - sx) * (x - sx) + (y - sy) * (y - sy);
+            return result;
+        }
+        if (direction.equals("UP")) {
+            if (x == sx && sy < y) {
+                return Integer.MAX_VALUE;
+            }
+            sy = 2 * max_n - sy;
+            int result = (x - sx) * (x - sx) + (y - sy) * (y - sy);
+            return result;
+        }
+        if (direction.equals("BOTTOM")) {
+            if (x == sx && sy > y) {
+                return Integer.MAX_VALUE;
+            }
+            sy = -sy;
+            int result = (x - sx) * (x - sx) + (y - sy) * (y - sy);
+            return result;
+        }
+        return 0;
     }
 }
