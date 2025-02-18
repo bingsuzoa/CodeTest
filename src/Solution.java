dde58161e6f61fa83ca771d615416fa33eafadae
bingsuzoa <zxc_777@naver.com>
2025-02-18 11:17:53 +0900

    #35 25.02.18 당구연습

--- src/Solution.java
+++ src/Solution.java
@@ -1,29 +1,67 @@
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class Solution {
+    static int max_m;
+    static int max_n;
+    static int min = Integer.MAX_VALUE;
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        StringTokenizer st;
-
-        String input = sc.nextLine();
-        st = new StringTokenizer(input);
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-
-        int count = 0;
-        while(true) {
-            int target = (n / k) * k;
-            count += (n - target);
-            n = target;
-            if(n < k) {
-                break;
-            }
-            n /= k;
-            count++;
+        Solution test = new Solution();
+        int[][] balls = {{7, 7}, {2, 7}, {7, 3}};
+        int[] answer = test.solution(10, 10, 3, 7, balls);
+        for (int value : answer) {
+            System.out.println(value);
         }
-        System.out.println(count - 1);
+    }
+
+    public int[] solution(int m, int n, int startX, int startY, int[][] balls) {
+        max_m = m;
+        max_n = n;
+        int[] answer = new int[balls.length];
+
+        for (int i = 0; i < balls.length; i++) {
+            min = Integer.MAX_VALUE;
+            min = Math.min(min, measure(startX, startY, balls[i][0], balls[i][1], "LEFT"));
+            min = Math.min(min, measure(startX, startY, balls[i][0], balls[i][1], "RIGHT"));
+            min = Math.min(min, measure(startX, startY, balls[i][0], balls[i][1], "UP"));
+            min = Math.min(min, measure(startX, startY, balls[i][0], balls[i][1], "BOTTOM"));
+            answer[i] = min;
+        }
+        return answer;
+    }
+
+    public int measure(int sx, int sy, int x, int y, String direction) {
+        if (direction.equals("LEFT")) {
+            if(y == sy) {
+                return Integer.MAX_VALUE;
+            }
+            sx = -sx;
+            int result = (x - sx) * (x - sx) + (y - sy) * (y - sy);
+            return result;
+        }
+        if (direction.equals("RIGHT")) {
+            if(y == sy) {
+                return Integer.MAX_VALUE;
+            }
+            sx = 2 * (max_n - sx) + sx;
+            int result = (x - sx) * (x - sx) + (y - sy) * (y - sy);
+            return result;
+        }
+        if (direction.equals("UP")) {
+            if(x == sx) {
+                return Integer.MAX_VALUE;
+            }
+            sy = 2 * (max_m - sy) + sy;
+            int result = (x - sx) * (x - sx) + (y - sy) * (y - sy);
+            return result;
+        }
+        if (direction.equals("BOTTOM")) {
+            if(x == sx) {
+                return Integer.MAX_VALUE;
+            }
+            sy = -sy;
+            int result = (x - sx) * (x - sx) + (y - sy) * (y - sy);
+            return result;
+        }
+        return 0;
     }
 }
