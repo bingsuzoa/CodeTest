cda57a3449a7fef97484f816980628aba870a631
bingsuzoa <zxc_777@naver.com>
2025-02-19 15:45:29 +0900

    #35 25.02.19 왕실의 나이트 : 어떤 char c가 알파벳에서 어떤 순서인지 알고 싶다면 ? int number = c - 'a', char를 int로 바꾸려면 - '0'

--- src/Solution.java
+++ src/Solution.java
@@ -1,150 +1,53 @@
 import java.util.*;
 
 public class Solution {
-    static boolean answer = false;
-    static int m, n;
-    static int count;
+    static Queue<Node> queue = new LinkedList<>();
 
     public static void main(String[] args) {
-        int[][] key = {{1,1},{1,1}};
-        int[][] lock = {{0,0,0},{0,0,0},{0,0,0}};
         Solution test = new Solution();
-        System.out.println(test.solution(key, lock));
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.nextLine();
+        char before = input.charAt(0);
+        int n = before - 'a';
+        int m = input.charAt(1) - '1';
+        
+        int[][] graph = new int[8][8];
+
+        test.go(graph, m, n);
+
+        int count = 0;
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+            int x = node.x;
+            int y = node.y;
+            if (x >= 8 || y >= 8 || x < 0 || y < 0) {
+                continue;
+            }
+            count++;
+        }
+        System.out.println(count);
+
     }
 
-    public boolean solution(int[][] key, int[][] lock) {
-        m = key.length;
-        n = lock.length;
-
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                if(lock[i][j] == 0) {
-                    count++;
-                }
-            }
-        }
-
-        int[][] key_90 = rotate(key);
-        int[][] key_180 = rotate(key_90);
-        int[][] key_270 = rotate(key_180);
-        List<int[][]> keyList = new ArrayList<>(4);
-        keyList.add(key);
-        keyList.add(key_90);
-        keyList.add(key_180);
-        keyList.add(key_270);
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                for (int[][] nowKey : keyList) {
-                    if (move_fromLU(nowKey, lock, i, j)) {
-                        answer = true;
-                        break;
-                    }
-                    if (move_fromRD(nowKey, lock, i, j)) {
-                        answer = true;
-                        break;
-                    }
-                    if (move_fromRU(nowKey, lock, i, j)) {
-                        answer = true;
-                        break;
-                    }
-                    if (move_fromLD(nowKey, lock, i, j)) {
-                        answer = true;
-                        break;
-                    }
-                }
-            }
-        }
-        return answer;
+    public void go(int[][] graph, int m, int n) {
+        queue.add(new Node(m - 1, n + 2));
+        queue.add(new Node(m - 1, n - 2));
+        queue.add(new Node(m + 1, n + 2));
+        queue.add(new Node(m + 1, n - 2));
+        queue.add(new Node(m + 2, n + 1));
+        queue.add(new Node(m + 2, n - 1));
+        queue.add(new Node(m - 2, n + 1));
+        queue.add(new Node(m - 2, n - 1));
     }
+}
 
-    public boolean move_fromLU(int[][] key, int[][] lock, int lock_x, int lock_y) {
-        int homeCount = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < m; j++) {
-                if (j + lock_y >= n || i + lock_x >= n) continue;
-                if (lock[i + lock_x][j + lock_y] == 1 && key[i][j] == 1) return false;
-                if (lock[i + lock_x][j + lock_y] == 0) {
-                    if (key[i][j] == 0) {
-                        return false;
-                    }
-                    homeCount++;
-                }
-            }
-        }
-        if(homeCount == count) {
-            return true;
-        }
-        return false;
-    }
+class Node {
+    int x;
+    int y;
 
-    public boolean move_fromRU(int[][] key, int[][] lock, int lock_x, int lock_y) {
-        int homeCount = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = m - 1; j >= 0; j--) {
-                if (j - m + 1 < 0 || j - m + 1 + lock_y >= n || i + lock_x >= n) continue;
-                if (lock[i + lock_x][j - m + 1 + lock_y] == 1 && key[i][j] == 1) return false;
-                if (lock[i + lock_x][j - m + 1 + lock_y] == 0) {
-                    if (key[i][j] == 0) {
-                        return false;
-                    }
-                    homeCount++;
-                }
-            }
-        }
-        if(homeCount == count) {
-            return true;
-        }
-        return false;
-    }
-
-    public boolean move_fromRD(int[][] key, int[][] lock, int lock_x, int lock_y) {
-        int homeCount = 0;
-        for (int i = m - 1; i >= 0; i--) {
-            for (int j = m - 1; j >= 0; j--) {
-                if (i - lock_x < 0 || j - lock_y < 0) continue;
-                if (lock[i - lock_x][j - lock_y] == 1 && key[i][j] == 1) return false;
-                if (lock[i - lock_x][j - lock_y] == 0) {
-                    if (key[i][j] == 0) {
-                        return false;
-                    }
-                    homeCount++;
-                }
-            }
-        }
-        if(homeCount == count) {
-            return true;
-        }
-        return false;
-    }
-
-    public boolean move_fromLD(int[][] key, int[][] lock, int lock_x, int lock_y) {
-        int homeCount = 0;
-        for (int i = m - 1; i >= 0; i--) {
-            for (int j = 0; j < m; j++) {
-                if (j - m + 1 < 0 || j - m + 1 + lock_x >= n || j + lock_y >= n) continue;
-                if (lock[j - m + 1 + lock_x][j + lock_y] == 1 && key[i][j] == 1) return false;
-                if (lock[j - m + 1 + lock_x][j + lock_y] == 0) {
-                    if (key[i][j] == 0) {
-                        return false;
-                    }
-                    homeCount++;
-                }
-            }
-        }
-        if(homeCount == count) {
-            return true;
-        }
-        return false;
-    }
-
-    public int[][] rotate(int[][] key) {
-        int[][] newKey = new int[m][m];
-        for (int i = 0; i < m; i++) {
-            for (int j = m - 1; j >= 0; j--) {
-                newKey[i][m - 1 - j] = key[j][i];
-            }
-        }
-        return newKey;
+    Node(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 }
