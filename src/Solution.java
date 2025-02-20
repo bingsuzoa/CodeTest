3e9003eae54ecb10a84c7ab0dbbf14222d824f0b
bingsuzoa <zxc_777@naver.com>
2025-02-20 15:01:11 +0900

    #35 25.02.20 자물쇠와 열쇠 : outOfIndex 다시 확인할것

--- src/Solution.java
+++ src/Solution.java
@@ -1,64 +1,101 @@
+
 import java.util.*;
 
 public class Solution {
-    static int[][] graph;
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
-    static int x, y, direction;
-
-    public static void turn_left() {
-        direction -= 1;
-        if(direction == -1) {
-            direction = 3;
-        }
-    }
+    static boolean answer = false;
+    static int m, n;
+    static List<int[]> list;
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        int[][] key = {{1, 1}, {1, 1}};
+        int[][] lock = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+        Solution test = new Solution();
+        System.out.println(test.solution(key, lock));
+    }
 
-        int m = sc.nextInt();
-        int n = sc.nextInt();
+    public boolean solution(int[][] key, int[][] lock) {
+        m = key.length;
+        n = lock.length;
 
-        x = sc.nextInt();
-        y = sc.nextInt();
-        direction = sc.nextInt();
-
-        graph = new int[m][n];
-        for (int i = 0; i < m; i++) {
+        list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                graph[i][j] = sc.nextInt();
+                if (lock[i][j] == 0) {
+                    list.add(new int[]{i, j});
+                }
             }
         }
-        graph[x][y] = 2;
-        int cnt = 0;
-        int block = 0;
-        while(true) {
-            turn_left();
-            int nx = x + dx[direction];
-            int ny = y + dy[direction];
-            if(graph[nx][ny] == 0) {
-                block = 0;
-                x  = nx;
-                y = ny;
-                graph[nx][ny] = 2;
-                cnt++;
-            } else {
-                block++;
-                if(block == 4) {
-                    nx = x - dx[direction];
-                    ny = y - dx[direction];
-                    if(graph[nx][ny] == 1) {
-                        System.out.println(cnt);
-                        break;
-                    } else {
-                        cnt++;
-                        x = nx;
-                        y = ny;
-                        block = 0;
+
+        int[][] key_90 = rotate(key);
+        int[][] key_180 = rotate(key_90);
+        int[][] key_270 = rotate(key_180);
+        List<int[][]> keyList = new ArrayList<>(4);
+        keyList.add(key);
+        keyList.add(key_90);
+        keyList.add(key_180);
+        keyList.add(key_270);
+
+        for(int i = 0; i < keyList.size(); i++) {
+            for(int j = m-1; j >= 0; j--) {
+                if(move(keyList.get(i), lock, j, m-1, 0,0)) {
+                    answer = true;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public boolean move(int[][] key, int[][] lock, int key_x, int key_y, int lock_x, int lock_y) {
+        int matchCount = 0;
+
+        if(key_y < 0 || lock_y >= n) {
+            return false;
+        }
+
+        if (key_y > 0) {
+            int count = 0;
+            for (int i = key_x; i < m; i++) {
+                for (int j = key_y; j < m; j++) {
+                    if (lock[m - 1 - i][count] == 1 && key[i][j] == 1
+                            || lock[m - 1 - i][count] == 0 && key[i][j] == 0) {
+                        move(key, lock, key_x, key_y - 1, lock_x, lock_y);
+                    }
+                    if (lock[m - 1 - i][count] == 0) {
+                        for(int k = 0; k < list.size(); k++) {
+                            int[] arr = list.get(k);
+                            if(m-1-i == arr[0] && count == arr[1]) {
+                                count++;
+                                matchCount++;
+                            }
+                        }
                     }
                 }
             }
         }
+        else {
+            int twoCount = 0;
+            for(int i = key_x; i < m; i++) {
+                for(int j = key_y; j < m; j++) {
+                    if (lock[m - 1 - i][lock_y + twoCount] == 1 && key[i][j] == 1
+                            || lock[m - 1 - i][lock_y + twoCount] == 0 && key[i][j] == 0) {
+                        move(key, lock, key_x, key_y, lock_x, lock_y + 1);
+                    }
+                }
+            }
+        }
+
+        if(matchCount == list.size()) return true;
+        return false;
+    }
+
+    public int[][] rotate(int[][] key) {
+        int[][] newKey = new int[m][m];
+        for (int i = 0; i < m; i++) {
+            for (int j = m - 1; j >= 0; j--) {
+                newKey[i][m - 1 - j] = key[j][i];
+            }
+        }
+        return newKey;
     }
 }
-
